61574ee66040196c178db8eb72fe414d6ce4b053
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-26 23:55:53 +0300

    added redux store

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import reducers from "./reducers/reducers";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { createStore } from "redux";
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunk)
+)
+
+export default store
