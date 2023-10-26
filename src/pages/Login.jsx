8f59151156a821b420a60d0f0e0f386114aa6dfa
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-26 23:58:24 +0300

    added login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Login() {
+    return (
+        <div className="login--page">
+            <div className="login--page--container">
+                <form>
+                    <select>
+                        
+                    </select>
+                </form>
+            </div>
+        </div>
+    )
+}
