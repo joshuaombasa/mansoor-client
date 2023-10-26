3e500fc6039b3fdb5a222ace642dac5cfacea3c9
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-26 23:58:41 +0300

    added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Home() {
+    return (
+        <div className="home--page">
+            <div className="home--page--container">
+                <h1>Welcome to Mansoor Construction Equipment Rentals.Your Trusted Partner for Reliable Construction Equipment Solutions</h1>
+                <p>At Mansoor, we understand the critical role that top-quality equipment plays in the success of your construction projects. With our wide range of meticulously maintained construction machinery and tools, we are committed to providing you with the resources you need to get the job done efficiently and effectively.</p>
+                <Link>Checkout Our Equipment</Link>
+            </div>
+        </div>
+    )
+}
