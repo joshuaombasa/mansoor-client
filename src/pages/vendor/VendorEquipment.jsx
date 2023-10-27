250abf4607df378147c8b544160df4073139532f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-27 23:52:00 +0300

    updated vendor equipment  page

--- src/pages/vendor/VendorEquipment.jsx
+++ src/pages/vendor/VendorEquipment.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { getVendorMachines } from "../../redux/action-creators/getVendorMachines";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function VendorEquipment() {
+    const [loading, setLoading] = useState(true)
     const dispatch = useDispatch()
     const vendorEquipment = useSelector((state) => state.vendorEquipment)
-    
+
     React.useEffect(() => {
+        setLoading(true)
         dispatch(getVendorMachines())
-    },[])
-    
-    console.log(vendorEquipment)
+        setLoading(false)
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="vendor--equipment--page">
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+
+    const equipmentElements = vendorEquipment.map(item => (
+        <div>
+            <Link to={item.id} className="vendor-equipment-item">
+                <img src={item.imageUrl} alt="" />
+                <div>
+                    <h3>{item.name}</h3>
+                    <span>Ksh {item.price}/day</span>
+                </div>
+            </Link>
+        </div>
+    ))
+
     return (
         <div className="vendor--equipment--page">
+            <h1>Your listed equipment</h1>
             <div className="vendor--equipment--page--container">
-               <h1>This is vendor equipment page</h1>
-               <button 
-                   onClick={() => dispatch(getVendorMachines())}
-               >Get vendor equipment</button>
+                {equipmentElements}
             </div>
         </div>
     )
