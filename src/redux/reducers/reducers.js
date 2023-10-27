f6ef3d8cab87c1b9011902eed695f7c608caaec1
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-27 23:49:56 +0300

    updated reducer

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import machinesReducer from "./machinesReducer";
 import vendorMachinesReducer from "./vendorMachinesReducer";
 import selectedMachineReducer from "./selectedMachineReducer";
+import selectedVendorMachineReducer from "./selectedVendorMachineReducer";
 
  const reducers = combineReducers({
     equipment: machinesReducer,
     vendorEquipment: vendorMachinesReducer,
     selectedEquipment: selectedMachineReducer,
+    selectedVendorEquipment: selectedVendorMachineReducer
 })
 
 export default reducers
