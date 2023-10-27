ff9c15ac36e3e53472451cdf3e3659ce9a4b1977
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-27 23:51:40 +0300

    updated income  page

---
+++ src/pages/vendor/Income.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Income() {
+    return (
+        <div className="income--page">
+            <div className="income--page--container">
+                <h1>This is income page</h1>
+            </div>
+        </div>
+    )
+}
