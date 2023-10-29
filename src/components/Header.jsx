97bac587cae79061284ead480078735f49ba1f54
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-29 23:11:11 +0300

    added logout link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import logoutUser from "../redux/action-creators/logoutUser";
+import { useDispatch } from "react-redux";
 
 export default function Header() {
+
+    const dispatch = useDispatch()
+
+    function handleLogout() {
+        dispatch(logoutUser())
+    }
+
     return (
         <header>
             <Link className="logo">mansoor</Link>
@@ -10,6 +19,10 @@ export default function Header() {
                 <NavLink to="/equipment">Machinery</NavLink>
                 <NavLink to="/vendor">Vendor</NavLink>
                 <NavLink to="/login">Login</NavLink>
+                <button 
+                   onClick={handleLogout} 
+                   className="logout--btn"
+                >Logout</button>
             </nav>
         </header>
     )
