a33c789a249eb8239c329df1beefd06c5399f4ee
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-29 22:22:26 +0300

    added login reducer

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -3,12 +3,14 @@ import machinesReducer from "./machinesReducer";
 import vendorMachinesReducer from "./vendorMachinesReducer";
 import selectedMachineReducer from "./selectedMachineReducer";
 import selectedVendorMachineReducer from "./selectedVendorMachineReducer";
+import loginReducer from "./loginReducer";
 
  const reducers = combineReducers({
     equipment: machinesReducer,
     vendorEquipment: vendorMachinesReducer,
     selectedEquipment: selectedMachineReducer,
-    selectedVendorEquipment: selectedVendorMachineReducer
+    selectedVendorEquipment: selectedVendorMachineReducer,
+    loggedInData : loginReducer
 })
 
 export default reducers
