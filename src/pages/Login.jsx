aa90ec02d71cde73ac4b9cad00c4d70d58a50068
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-29 22:26:23 +0300

    updated login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,75 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import loginUser from "../redux/action-creators/loginUser";
+import { loginVendor } from "../api";
 
 export default function Login() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const location = useLocation()
+   const message = location.state ? location.state.message : ""
+
+    const [error, setError] = useState("")
+
+    
+    const loggedInData = useSelector(state => state.loggedInData)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setFormData((prevFormData) => (
+            {
+                ...prevFormData,
+                [name] : value
+            }
+        ))
+    }
+    
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        async function doLogin() {
+            try {
+                const data = await loginVendor(formData)
+                dispatch(loginUser(data.user))
+                navigate("/vendor")
+            } catch (error) {
+                setError(error)
+            }
+        }
+        doLogin()
+    }
+
     return (
         <div className="login--page">
             <div className="login--page--container">
-                <form>
-                    <select>
-                        
-                    </select>
+                <h1>Sign in to your account</h1>
+                {message && <p className="error">{message}</p>}
+                {error && <p className="error">{error.message}</p>}
+                <form onSubmit={handleSubmit} className="login--form">
+                    <input 
+                         type="email" 
+                         name="email"
+                         value={formData.email}
+                         onChange={handleChange }
+                         placeholder="Email address"
+                    />
+                    <input 
+                         type="password" 
+                         name="password"
+                         value={formData.password}
+                         onChange={handleChange}
+                         placeholder="Password"
+                    />
+                    <button>Sign in</button>
                 </form>
+                <span>Don’t have an account? <Link>Create one now</Link></span>
             </div>
         </div>
     )
