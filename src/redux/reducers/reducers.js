43544628cf357de49ef08c1dc8ffe10820263353
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-30 18:13:24 +0300

    added reducers

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -4,13 +4,19 @@ import vendorMachinesReducer from "./vendorMachinesReducer";
 import selectedMachineReducer from "./selectedMachineReducer";
 import selectedVendorMachineReducer from "./selectedVendorMachineReducer";
 import loginReducer from "./loginReducer";
+import vendorReviewsReducer from "./vendorReviewsReducer";
+import vendorIncomeReducer from "./vendorIncomeReducer";
+import vendorTransactionsReducer from "./vendorTransactionsReducer";
 
  const reducers = combineReducers({
     equipment: machinesReducer,
     vendorEquipment: vendorMachinesReducer,
     selectedEquipment: selectedMachineReducer,
     selectedVendorEquipment: selectedVendorMachineReducer,
-    loggedInData : loginReducer
+    loggedInData : loginReducer,
+    incomeList: vendorIncomeReducer,
+    transactionsData: vendorTransactionsReducer,
+    reviews: vendorReviewsReducer
 })
 
 export default reducers
