4db7ae64da8fc239f0be57b2e1013588286f2e1c
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-30 18:15:04 +0300

    updated  action creator

--- src/redux/action-creators/getVendorMachines.js
+++ src/redux/action-creators/getVendorMachines.js
@@ -1,12 +1,8 @@
-export function getVendorMachines() {
+export function getVendorMachines(data) {
     return (dispatch) => {
-    fetch("http://localhost:3000/api/vendor/equipment")
-         .then(res => res.json())
-         .then(data => {
-            dispatch({
-                type: "GET_VENDOR_MACHINES",
-                payload: data
-            })
-         })
+        dispatch({
+            type: "GET_VENDOR_MACHINES",
+            payload: data
+        })
     }
 }
