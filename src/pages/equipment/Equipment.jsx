7d49cde03572a9d9efbac4b1ee6eb71b62098e91
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-30 18:17:52 +0300

    updated page logic

--- src/pages/equipment/Equipment.jsx
+++ src/pages/equipment/Equipment.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getallMachines } from "../../redux/action-creators/machines";
+
 import EquipmentItem from "../../components/EquipmentItem";
+import { getAllEquipment } from "../../api";
 
 export default function Equipment() {
     const [category, setCategory] = useState("")
+    const [loading, setLoading] = React.useState(true)
+    const [error, setError] = useState("")
+
 
     function handleCatgoryChange(event) {
-        const {name, value} = event.target
+        const { name, value } = event.target
         setCategory(value)
     }
 
@@ -19,16 +24,29 @@ export default function Equipment() {
 
     const equipment = useSelector(state => state.equipment)
     const dispatch = useDispatch()
-    const [loading, setLoading] = React.useState(false)
 
-    React.useEffect(() => {
-        setLoading(true)
-        dispatch(getallMachines())
-        setLoading(false)
+
+    // React.useEffect(() => {
+    //     setLoading(true)
+    //     dispatch(getallMachines())
+    //     setLoading(false)
+    // }, [])
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const data = await getAllEquipment()
+                dispatch(getallMachines(data))
+                console.log(data)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        getData()
     }, [])
-
-
-
 
     if (loading) {
         return (
@@ -38,11 +56,20 @@ export default function Equipment() {
         )
     }
 
+    if (error) {
+        return (
+            <>
+                <h1>Error: {error.message}</h1>
+                <pre>{error.status} - {error.statusText}</pre>
+            </>
+        )
+    }
+
     const filteredEquipment = category !== ""
-                              ? equipment.filter(item => item.name === category) 
-                              : equipment
-   
-        
+        ? equipment.filter(item => item.name === category)
+        : equipment
+
+
 
     const equipmentElements = filteredEquipment.map(item => (
         <EquipmentItem key={item.id} item={item} />
@@ -54,16 +81,16 @@ export default function Equipment() {
             <h1>Explore our equiment</h1>
             <div className="filter--section">
                 <span>Filter by type</span>
-            <select
-                value={category}
-                onChange={handleCatgoryChange}
-            >
-                <option value="">-- Choose --</option>
-                {equipment.map(item => (
-                    <option key={item.id} value={item.name}>{item.name}</option>
-                ))}
-            </select>
-            <button onClick={clearFilters}>Clear filters</button>
+                <select
+                    value={category}
+                    onChange={handleCatgoryChange}
+                >
+                    <option value="">-- Choose --</option>
+                    {equipment.map(item => (
+                        <option key={item.id} value={item.name}>{item.name}</option>
+                    ))}
+                </select>
+                <button onClick={clearFilters}>Clear filters</button>
             </div>
             <div className="equipment--page--container">
                 {equipmentElements}
