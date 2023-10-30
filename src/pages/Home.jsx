f57a8b4eec536c65435fee798feafd8daf7eeed5
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-30 18:15:55 +0300

    updated page logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="home--page--container">
                 <h1>Welcome to Mansoor Construction Equipment Rentals.Your Trusted Partner for Reliable Construction Equipment Solutions</h1>
                 <p>At Mansoor, we understand the critical role that top-quality equipment plays in the success of your construction projects. With our wide range of meticulously maintained construction machinery and tools, we are committed to providing you with the resources you need to get the job done efficiently and effectively.</p>
-                <Link>Checkout Our Equipment</Link>
+                <Link to="/equipment">Checkout Our Equipment</Link>
             </div>
         </div>
     )
