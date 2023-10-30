bbbf34ea0c0fd6dbb94e5219d6c9028d7c74e131
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-30 18:17:43 +0300

    updated page logic

--- src/pages/equipment/EquipmentDetails.jsx
+++ src/pages/equipment/EquipmentDetails.jsx
@@ -1,26 +1,35 @@
-import React from "react";
-import { Link , useParams} from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useParams } from "react-router-dom";
 import getSelectedMachine from "../../redux/action-creators/getSelectedMachine";
 import { useDispatch, useSelector } from "react-redux";
+import { getSelectedEquipment } from "../../api";
 
 export default function EquipmentDetails() {
 
     const [loading, setLoading] = React.useState(false)
+
+    const [error, setError] = useState("")
 
     const dispatch = useDispatch()
     const selectedEqupment = useSelector((state) => state.selectedEquipment)
 
     const params = useParams()
 
-    console.log(params)
-
     React.useEffect(() => {
-        setLoading(true)
-        dispatch(getSelectedMachine(params.id))
-        setLoading(false)
-    }, [])
+        async function getData() {
+            try {
+                setLoading(true)
+                const data = await getSelectedEquipment(params.id)
+                dispatch(getSelectedMachine(data))
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
 
-    console.log(selectedEqupment)
+        getData()
+    }, [])
 
     if (loading) {
         return (
@@ -30,7 +39,16 @@ export default function EquipmentDetails() {
         )
     }
 
-    function renderDetails () {
+    if (error) {
+        return (
+            <>
+                <h1>Error: {error.message}</h1>
+                <pre>{error.status} - {error.statusText}</pre>
+            </>
+        )
+    }
+
+    function renderDetails() {
         return (
             <div className="equipment--details--container">
                 <img src={selectedEqupment.imageUrl} alt="" />
@@ -45,9 +63,9 @@ export default function EquipmentDetails() {
     return (
         <div className="equipment-details--page">
             <Link
-              to={`..`}
-              relative="path"
-              className="back--button"
+                to={`..`}
+                relative="path"
+                className="back--button"
             >&larr; <span>Back to all equipment</span></Link>
             {selectedEqupment && renderDetails()}
         </div>
