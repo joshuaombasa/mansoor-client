def92b667a4da92a7b17ba5f54a1587208366f81
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-30 18:14:50 +0300

    updated  action creator

--- src/redux/action-creators/machines.js
+++ src/redux/action-creators/machines.js
@@ -1,12 +1,9 @@
-export function getallMachines() {
+export function getallMachines(data) {
     return (dispatch) => {
-    fetch("http://localhost:3000/api/equipment")
-         .then(res => res.json())
-         .then(data => {
-            dispatch({
-                type: "GET_ALL_MACHINES",
-                payload: data
-            })
-         })
+    
+        dispatch({
+            type: "GET_ALL_MACHINES",
+            payload: data
+        })
     }
 }
